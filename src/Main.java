18666a65fca0803fbf8ea61999eafc525993b43b
MaxDezan <maxdezan@rede.ulbra.br>
2026-04-09 22:02:00 -0300

    Service 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import domain.Product;
 import service.ProductService;
+import utils.GenerateValue;
 
 void main() {
     ProductService productService = new ProductService();
 
     productService.create(new Product(
-            productService.generateUUID(),
+            GenerateValue.getUUID(),
             "SKU",
             "celular",
             2

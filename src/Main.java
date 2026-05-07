c61730a38a70b73f879136e6669a9fd7adebd28a
MaxDezan <maxdezan@rede.ulbra.br>
2026-05-07 12:15:11 -0300

    minor overall fixes

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,27 @@
+import domain.Price;
 import domain.Product;
 import service.ProductService;
+import utils.GenerateValue;
+
+import java.util.Date;
 
 void main() {
     ProductService productService = new ProductService();
 
-    Product produto = new Product("SKU-001", "Notebook", 2000f);
+    Product produto = new Product("SKU-001", "Notebook", new Price(4500f, new Date()));
     productService.create(produto);
 
-    Product produto2 = new Product("SKU-002", "Tablet", 1000f);
+    Product produto2 = new Product("SKU-002", "Tablet", new Price(2500f, new Date()));
     productService.create(produto2);
 
-    produto2.setPrice(1899.99f);
-    productService.create(produto2);
+    Product produto3 = new Product("SKU-003", "Mouse", new Price(300f, new Date()));
+    productService.create(produto3);
 
     produto.setPrice(5000.99f);
     productService.create(produto);
 
-    System.out.println("--- Products ---");
+    produto2.setPrice(1899.99f);
+    productService.create(produto2);
+
     productService.listAll();
 }
